719d017ca348f570c49a0dedde2dd07751bafffc
Markus Rothlübbers <markus@mrlb.space>
2021-06-08 19:46:13 +0200

    Implements better error handeling for bpm controller

--- RunToBeat.Api/Controllers/BpmController.cs
+++ RunToBeat.Api/Controllers/BpmController.cs
@@ -10,7 +10,7 @@ using RunToBeat.Api.Services;
 namespace RunToBeat.Api.Controllers
 {
     /// <summary>
-    /// Implements a controller for bpm interaction
+    ///     Implements a controller for bpm interaction
     /// </summary>
     [ApiController]
     [Route("[controller]")]
@@ -20,33 +20,52 @@ namespace RunToBeat.Api.Controllers
         private readonly IHappiDevMusicService _musicService;
 
         /// <summary>
-        /// Creates a new <see cref="BpmController"/>-object.
+        ///     Creates a new <see cref="BpmController" />-object.
         /// </summary>
         /// <param name="logger"></param>
         /// <param name="musicService"></param>
-        public BpmController(ILogger<BpmController> logger, IHappiDevMusicService musicService)
+        public BpmController(ILogger<BpmController> logger,
+            IHappiDevMusicService musicService)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _musicService = musicService ?? throw new ArgumentNullException(nameof(musicService));
         }
 
         /// <summary>
-        /// Gets a list of tracks by bpms
+        ///     Gets a list of tracks by bpms
         /// </summary>
         /// <param name="bpm">the requested bpm</param>
         /// <returns>a list of tracks with the requested bpm</returns>
         [HttpGet]
         public async Task<ActionResult<IEnumerable<TrackModel>>> GetTracksByBpm(string bpm)
         {
-            _logger.LogInformation("Getting tracks for tempo {Bpm}", bpm);
-            var tracks = await _musicService.GetTracksByBpm(bpm);
-            if (!tracks.Any())
+            using (_logger.BeginScope("Getting tracks for tempo {Bpm}", bpm))
             {
-                return NoContent();
-            }
+                IEnumerable<TrackModel> tracks;
+                try
+                {
+                    tracks = await _musicService.GetTracksByBpm(bpm);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e, "Error while fetching bpms");
+                    var problem = ProblemDetailsFactory.CreateProblemDetails(HttpContext,
+                        400,
+                        "Error while fetching bpms",
+                        "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.1",
+                        e.Message);
+                    return BadRequest(problem);
+                }
 
-            return Ok(tracks);
+                if (!tracks.Any())
+                {
+                    _logger.LogInformation("No tracks found");
+                    return NoContent();
+                }
+
+                _logger.LogInformation("Returning tracks");
+                return Ok(tracks);
+            }
         }
-        
     }
 }
